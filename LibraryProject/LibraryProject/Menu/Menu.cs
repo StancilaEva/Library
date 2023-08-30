9db1adfada9b6c696f08c1604ef3f5df78172f05
Eva Stancila <eva.stancila@amdaris.com>
2023-08-30 22:11:05 +0300

    library service and menu

---
+++ LibraryProject/LibraryProject/Menu/Menu.cs
@@ -0,0 +1,89 @@
+﻿using LibraryProject.DTOs;
+using LibraryProject.Services;
+
+namespace LibraryProject.Menu
+{
+    public class Menu
+    {
+        private LibraryService libraryService;
+
+        public void ShowChoices()
+        {
+            Console.WriteLine("1. Add new book to the library");
+            Console.WriteLine("2. List all the library books");
+            Console.WriteLine("3. Show me how many are left of this book");
+            Console.WriteLine("4. Lend me a book");
+            Console.WriteLine("5. Give back book");
+            Console.WriteLine("6. Exit");
+            Console.WriteLine("Enter your choice: ");
+        }
+
+        public void RunMenu()
+        {
+            while (true)
+            {
+                ShowChoices();
+                var choice = ReadUserChoiceInput();
+
+                switch (choice)
+                {
+                    case 1:
+                        AddNewBookToLibraryChoice();
+                        break;
+                    case 2:
+                        ListAllTheBooks();
+                        break;
+                    default:
+                        {
+
+                        }
+                }
+            }
+        }
+
+        private void ListAllTheBooks()
+        {
+            Console.WriteLine();
+        }
+
+        private void AddNewBookToLibraryChoice()
+        {
+            double price; string isbn; string title;
+
+            Console.WriteLine("ISBN: ");
+            isbn = Console.ReadLine();
+
+            Console.WriteLine("Title: ");
+            title = Console.ReadLine();
+
+            Console.WriteLine("Price (Please enter a number): ");
+            var priceAsString = Console.ReadLine();
+
+            if (!double.TryParse(priceAsString, out price))
+            {
+                throw new Exception("Please provide a number!");
+            }
+
+            libraryService.AddBookToLibrary(new BookDetailsDTO()
+            {
+                ISBN = isbn,
+                Title = title,
+                Price = price
+            });
+        }
+
+        private int ReadUserChoiceInput()
+        {
+            int choiceNumber;
+            string input = Console.ReadLine();
+
+            while (!int.TryParse(input, out choiceNumber) || (choiceNumber < 0 && choiceNumber > 6))
+            {
+                Console.WriteLine("Please enter a number between 1 and 6)");
+                input = Console.ReadLine();
+            }
+
+            return choiceNumber;
+        }
+    }
+}
---
+++ LibraryProject/LibraryProject/Services/LibraryService.cs
@@ -0,0 +1,72 @@
+﻿using LibraryProject.Domain;
+using LibraryProject.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject.Services
+{
+    public class LibraryService
+    {
+        private List<Book> Books;
+        private List<BookLend> Lends;
+        public LibraryService()
+        {
+        }
+
+        public List<Book> GetAllLibraryBooks()
+        {
+            return Books;
+        }
+
+        public void AddBookToLibrary(BookDetailsDTO bookDetailsDTO)
+        {
+            CheckIfBookIsValid(bookDetailsDTO);
+
+            var existingBook = Books.FirstOrDefault(book => book.ISBN == bookDetailsDTO.ISBN);
+
+            if (existingBook != null)
+            {
+                existingBook.Prices.Add(bookDetailsDTO.Price);
+            }
+            else
+            {
+                Books.Add(new Book
+                {
+                    ISBN = bookDetailsDTO.ISBN,
+                    Title = bookDetailsDTO.Title,
+                    Prices = new List<double>() { bookDetailsDTO.Price },
+                });
+            }
+        }
+
+        public int CheckNumberOfBooksAvailable(string iSBN)
+        {
+            try
+            {
+                //trateaza cazul in care nu exista cartea!!!
+                var bookCount = Books.First(book => book.ISBN == iSBN).Prices.Count;
+
+                return bookCount - Lends.Where(lend => lend.BookISBN.Equals(iSBN)).Count();
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw new Exception("No such book with this ISBN");
+            }
+        }
+
+        private void CheckIfBookIsValid(BookDetailsDTO bookDetailsDTO)
+        {
+            if (string.IsNullOrEmpty(bookDetailsDTO.Title.Trim()))
+                throw new Exception("The Title cannot be empty");
+
+            if (string.IsNullOrEmpty(bookDetailsDTO.ISBN.Trim()))
+                throw new Exception("The ISBN cannot be empty");
+
+            if (bookDetailsDTO.Price <= 0)
+                throw new Exception("You have to add a price");
+        }
+    }
+}
